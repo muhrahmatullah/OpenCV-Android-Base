3d881a980788d592cab3a183786a2fcc9323ea8b
Muhammad Rahmatullah <muhammadrahmatullah1@gmail.com>
2017-12-04 00:57:18 +0800

    opencv works and add rgb2grayscale sample

--- app/src/main/java/com/rahmat/app/opencvandroid/MainActivity.java
+++ app/src/main/java/com/rahmat/app/opencvandroid/MainActivity.java
@@ -1,9 +1,23 @@
 package com.rahmat.app.opencvandroid;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView1;
+    Context context;
+    Bitmap img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +25,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.loadLibrary("opencv_java3");
+
+        imageView1 = findViewById(R.id.imageView1);
+
+        img = BitmapFactory.decodeResource(this.getResources(),R.drawable.bg_image);
+        imageView1.setImageBitmap(img);
+    }
+
+    public void toGray(View view) {
+
+        Mat source = new Mat();
+        Utils. bitmapToMat( img, source);
+        Mat dest = new Mat(source.size(), CvType.CV_8UC1);
+
+        Imgproc. cvtColor(source, dest, Imgproc.COLOR_RGB2GRAY, 4);
+        Bitmap btmp = Bitmap.createBitmap(dest.width(),dest.height(),Bitmap.Config.ARGB_8888);
+        Utils. matToBitmap(dest,btmp);
+        imageView1.setImageBitmap(btmp);
     }
 }
